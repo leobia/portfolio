552f02d1e0d800eb9b95db570206b8f117d70dbf
Leonardo Bianco <leonardo.bianco@outlook.com>
2019-08-07 22:27:20 +0200

    Added component skills

---
+++ components/Skills.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="hero is-white is-fullheight">
+    <div class="hero-body">
+      <div class="container" style="z-index: 1;">
+        <div class="columns is-vcentered">
+          <div class="column is-half has-text-centered">
+            <h2 class="subtitle is-2">Skills</h2>
+            <div>From the time I started my journey as a web developer, I have been constantly learning new things. In fact I'm always trying to expand my skillset by reading articles, watching videos and experimenting new technologies.</div>
+          </div>
+          <div class="column is-half">
+            <div class="box small-border-radius">
+              <h3 class="title is-6 has-text-centered">Back-end</h3>
+              <div class="tags">
+                <span class="tag is-dark" v-for="(skill, index) in backEndSkills" :key="index">{{skill}}</span>
+              </div>
+              <hr />
+              <h3 class="title is-6 has-text-centered">Front-end</h3>
+              <div class="tags">
+                <span class="tag is-dark" v-for="(skill, index) in frontEndSkills" :key="index">{{skill}}</span>
+              </div>
+              <hr />
+              <h3 class="title is-6 has-text-centered">Development Tools</h3>
+              <div class="tags">
+                <span class="tag is-dark" v-for="(skill, index) in devTools" :key="index">{{skill}}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+    .tag {
+        width: 6em;
+    }
+    .tags {
+        justify-content: center;
+    }
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      backEndSkills: ["Java", "Spring", "JEE", "JSF", "Python", "Django"],
+      frontEndSkills: ["ES6", "HTML", "CSS", "Vue", "jsp"],
+      devTools: ["IntelliJ", "VS Code", "Eclipse", "Netbeans"]
+    };
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -8,46 +8,21 @@
 
     <Timeline />
 
-    <section class="hero is-white is-fullheight">
-      <div class="hero-body">
-        <div class="container" style="z-index: 1;">
-          <div class="columns is-vcentered">
-            <div class="column is-half has-text-centered">
-              <h2 class="subtitle is-2">Skills</h2>
-              <div>
-                From the time I started my journey as a web developer, I have been constantly learning new things. In fact I'm always trying to expand my skillset by reading articles, watching videos and experimenting new technologies.
-              </div>
-            </div>
-            <div class="column is-half">
-              <div class="box small-border-radius">
-                <h3 class="title is-6 has-text-centered">Back-end</h3>
-                <hr>
-                <h3 class="title is-6 has-text-centered">Front-end</h3>
-                <hr>
-                <h3 class="title is-6 has-text-centered">Development Tools</h3>
-              </div>
-              <!-- <div class="box no-border-radius">
-                <h3 class="title is-6 has-text-centered">Front-end</h3>
-              </div>
-              <div class="box no-border-radius">
-                <h3 class="title is-6 has-text-centered">Development Tools</h3>
-              </div> -->
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <Skills />
+
   </div>
 </template>
 
 <script>
 import Timeline from "~/components/Timeline.vue";
 import Banner from "~/components/Banner.vue";
+import Skills from "~/components/Skills.vue";
 
 export default {
   components: {
     Timeline,
-    Banner
+    Banner,
+    Skills
   }
 };
 </script>
